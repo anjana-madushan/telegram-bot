634fa4a0304548c97f224a9eb8088f2d44615c90
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2025-01-04 00:20:00 +0530

    vercel configuration changed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import router from './routers/bot.js';
+import router from '../routers/bot.js';
 
 
 dotenv.config();
@@ -10,8 +10,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use('/genie', router);
 app.get('/', (req, res) => {
-  console.log("Telegram Bot Server is running.");
-  res.send("Telegram Bot Server is running.");
+  console.log("Genie Telegram Bot Server is running.");
+  res.send("Genie Telegram Bot Server is running.");
 });
 
 const PORT = process.env.PORT || 3000;
@@ -19,3 +19,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is listening to Port 3000`);
 })
+
+export default app;
